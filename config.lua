b8a3440eeaae102b0a69f05f4a034920de444264
asil <asilturkcandar39@gmail.com>
2025-08-10 18:27:18 +0300

    Add files via upload

--- config.lua
+++ config.lua
@@ -10,19 +10,19 @@ Config.AllowedJobs = {
     ["police"] = true,
 }
 
-Config.MDTCommand = 'mdt'            -- MDT açma komutu adı
-Config.MDTKey = 'F5'                 -- MDT açma tuşu
+Config.MDTCommand = 'mdt' 
+Config.MDTKey = 'F5' 
 
-Config.ZoomCommand = 'tabletzoom'    -- Zoom ayarlama komutu
+Config.ZoomCommand = 'tabletzoom'
 Config.ZoomKey = ''    
 
 Config.Logs = {
     enabled = true,
-    webhook = "https://discord.com/api/webhooks/1402106542467317880/s-00CLd4vlhLeDCYp8X26d4GMwSOAWDsroA5tsTkQoE7gTFdzMH4wXBz-qgYIdUbbgL-",
-    olaylar = true,        -- olay ekle, sil vs.
-    ceza = true,           -- ceza kaydı
-    sorgula = true,        -- isim/numara/plaka/arac sorgulama
-    sabika = true,         -- sabıka silme vs.
-    aranan = true,         -- arama ekle/kaldır
-    avatar = true,         -- avatar değişikliği
+    webhook = "",
+    olaylar = true, 
+    ceza = true,  
+    sorgula = true,       
+    sabika = true,         
+    aranan = true,         
+    avatar = true,        
 }
